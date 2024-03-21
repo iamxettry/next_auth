277cf221d96a9c2481aab0e74d0fdebd92c7f4ca
iamxettry <rajuchhetry11@gmail.com>
2024-03-21 15:10:54 +0545

    db added

--- src/actions/Register.ts
+++ src/actions/Register.ts
@@ -2,12 +2,31 @@
 
 import * as z from "zod"
 import {RegisterSchema} from '@/Schemas'
-
+import bcrypt from 'bcrypt'
+import { db } from "@/lib/db"
 export const register= async (values:z.infer<typeof RegisterSchema>)=>{
     const validateFields = RegisterSchema.safeParse(values)
     if(!validateFields.success){
         return {error:"Invalid fields"}
     }
+    const {email,password,name} = validateFields.data
+    const salt = await bcrypt.genSalt(10)
+    const hashPassowrd = await bcrypt.hash(password,salt)
+    const existingUser= await db.user.findUnique({
+        where:{
+            email,
+        }
+    });
+    if(existingUser){
+        return {error:"Email already in use!"}
 
-    return {success:"Email Sent!"}
+    }
+    await db.user.create({
+        data:{
+            name,
+            email,
+            password:hashPassowrd
+        }
+    })
+    return {success:"User Created!"}
 }
